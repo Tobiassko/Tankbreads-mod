71feab33f812d4b13d3a7dcb32a618c9358dde9f
Me <tobias.olofsson@edu.sgy.se>
2025-09-02 10:52:13 +0200

    commit

---
+++ src/main/java/tank/bread/mod/TankbreadModClient.java
@@ -0,0 +1,10 @@
+package tank.bread.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TankbreadModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
